ab90fe34626876c1b04e27e0734146da6938a0a5
gil7788 <gil7788@gmail.com>
2025-05-25 19:04:30 +0300

    Update Meme Coin Metadata

--- packages/foundry/contracts/MemeCoinFactory.sol
+++ packages/foundry/contracts/MemeCoinFactory.sol
@@ -9,6 +9,12 @@ contract MemeCoinFactory {
         string name;
         string symbol;
         string description;
+        string xProfile;
+        string telegram;
+        string youtubeLink;
+        string instagram;
+        string tiktok;
+        string webpage;
         string image; // base64 stored fully on-chain
     }
 
@@ -19,6 +25,12 @@ contract MemeCoinFactory {
         string memory name,
         string memory symbol,
         string memory description,
+        string memory xProfile,
+        string memory telegram,
+        string memory youtubeLink,
+        string memory instagram,
+        string memory tiktok,
+        string memory webpage,
         string memory image
     ) external returns (MemeCoin) {
         MemeCoin newToken = new MemeCoin(_maxCap, name, symbol);
@@ -27,13 +39,23 @@ contract MemeCoinFactory {
 
         address tokenAddr = address(newToken);
 
-        tokenMetadata[tokenAddr] =
-            TokenMetadata({ maxCap: _maxCap, name: name, symbol: symbol, description: description, image: image });
+        tokenMetadata[tokenAddr] = TokenMetadata({
+            maxCap: _maxCap,
+            name: name,
+            symbol: symbol,
+            description: description,
+            xProfile: xProfile,
+            telegram: telegram,
+            youtubeLink: youtubeLink,
+            instagram: instagram,
+            tiktok: tiktok,
+            webpage: webpage,
+            image: image
+        });
 
         return newToken;
     }
 
-    // Consider to delete
     function getTokenMetadata(address token) external view returns (TokenMetadata memory) {
         return tokenMetadata[token];
     }
